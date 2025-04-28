cbca99364a07703de901a8e67ef674f05255e5ea
Nicholas Ang <nicholas.ang@data4life-asia.care>
2025-04-28 16:30:16 +0800

    update celltypes

--- packages/starboard-notebook/dist/src/cellTypes/registry.js
+++ packages/starboard-notebook/dist/src/cellTypes/registry.js
@@ -32,7 +32,7 @@ export function getCellTypeDefinitionForCellType(cellType) {
 export function getAvailableCellTypes() {
     const cellTypes = [...new Set(registry.values())];
     // Hide "ES Module" cell type from user
-    return [...new Set(registry.values())];
+    return cellTypes.filter(cell => cell.name !== "ES Module");
 }
 // Set Python as default language
 export function getDefaultCellType() {
