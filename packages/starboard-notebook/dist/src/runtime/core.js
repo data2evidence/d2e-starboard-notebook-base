bd54bf46e2624046454c8ea771f50a779527be8b
Nicholas Ang <66071629+KangJingA@users.noreply.github.com>
2025-05-07 15:04:58 +0800

    Fix import starboard notebook (#56)
    * remove hardcode
    
    * add build files

--- packages/starboard-notebook/dist/src/runtime/core.js
+++ packages/starboard-notebook/dist/src/runtime/core.js
@@ -89,12 +89,12 @@ export function setupCommunicationWithParentFrame(runtime) {
                     await nb.notebookInitialize();
                     // Finding the Starboard Cells
                     const sbCells = document.querySelectorAll("starboard-cell");
-                    const pyqeInitCell = sbCells[0];
-                    const jupyterInitCell = sbCells[1];
+                    const pyqeInitCell = sbCells[sbCells.length - 3];
+                    const jupyterInitCell = sbCells[sbCells.length - 2];
                     // Run and remove cells
                     await (pyqeInitCell === null || pyqeInitCell === void 0 ? void 0 : pyqeInitCell.runtime.controls.runCell({ id: pyqeInitCell.id, type: "install" }));
-                    await (pyqeInitCell === null || pyqeInitCell === void 0 ? void 0 : pyqeInitCell.runtime.controls.removeCell({ id: pyqeInitCell.id }));
                     await (jupyterInitCell === null || jupyterInitCell === void 0 ? void 0 : jupyterInitCell.runtime.controls.runCell({ id: jupyterInitCell.id }));
+                    await (pyqeInitCell === null || pyqeInitCell === void 0 ? void 0 : pyqeInitCell.runtime.controls.removeCell({ id: pyqeInitCell.id }));
                     await (jupyterInitCell === null || jupyterInitCell === void 0 ? void 0 : jupyterInitCell.runtime.controls.removeCell({ id: jupyterInitCell.id }));
                     await nb.performUpdate();
                     const notebookEl = document.querySelector("starboard-notebook");
